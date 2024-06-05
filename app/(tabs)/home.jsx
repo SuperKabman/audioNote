b70213a15476c52b730f418463d78b9dc68939f2
Kabir <109855930+SuperKabman@users.noreply.github.com>
2024-06-04 23:15:59 -0700

    refactor: Update position of "Record" text in home component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         onPress={handlePress}
         style={{ alignItems: 'center', justifyContent: 'center' }}>
         <Image source={require('@/assets/images/blob_1.gif')} style = {{width: '70%', height: '70%',top:'50%'}} />
-        <Text style={{position:'absolute', top:'94%', left:145,color: 'white', fontSize: 36 }}>Record</Text>
+        <Text style={{position:'absolute', top:'94%', left:'37%',color: 'white', fontSize: 36 }}>Record</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={handleProfile}>
