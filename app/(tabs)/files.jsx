e1490565c4b3368c4d7cf4203db00fb34c452250
Siddharth <siddharth23525@iiitd.ac.in>
2024-06-14 13:24:54 +0530

    added delete and share functionality to the files page

--- app/(tabs)/files.jsx
+++ app/(tabs)/files.jsx
@@ -4,16 +4,22 @@ import {
   SafeAreaView,
   TouchableOpacity,
   StyleSheet,
+  ScrollView,
+  Alert
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
 import { useNavigation } from "@react-navigation/native";
-
-
+import { Menu, MenuOptions, MenuOption, MenuTrigger, MenuProvider } from 'react-native-popup-menu';
+import { Ionicons } from '@expo/vector-icons';
+import JSZip from "jszip";
 
 const Files = () => {
   const [directories, setDirectories] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
   const navigation = useNavigation();
+
   useEffect(() => {
     listDirectories();
   }, []);
@@ -58,15 +64,56 @@ const Files = () => {
     console.log(`Directory ${path} clicked`);
   };
 
-  const deleteAllFiles = async () => {
-    try {
-      const recordingsPath = FileSystem.documentDirectory + "recordings";
-      await FileSystem.deleteAsync(recordingsPath, { idempotent: true });
-      console.log('All files in recordings directory have been deleted');
-      listDirectories(); // Refresh the list of directories
-    } catch (error) {
-      console.error('Failed to delete files', error);
+  const handleLongPress = (directory) => {
+    setSelectedItems((prevSelectedItems) => {
+      if (prevSelectedItems.includes(directory.path)) {
+        return prevSelectedItems.filter(item => item !== directory.path);
+      } else {
+        return [...prevSelectedItems, directory.path];
+      }
+    });
+  };
+
+  const handleMenuDelete = async () => {
+    for (const item of selectedItems) {
+      try {
+        await FileSystem.deleteAsync(item, { idempotent: true });
+        console.log(`Deleted ${item}`);
+      } catch (error) {
+        console.error(`Failed to delete ${item}`, error);
+      }
     }
+    setSelectedItems([]);
+    listDirectories(); // Refresh the list of directories
+  };
+
+  const handleMenuShare = async () => {
+    try {
+      const zip = new JSZip();
+
+      for (const directoryPath of selectedItems) {
+        const items = await FileSystem.readDirectoryAsync(directoryPath);
+        for (const item of items) {
+          if (item.endsWith('.m4a') || item.endsWith('.wav') || item === 'summary.txt') {
+            const itemPath = `${directoryPath}/${item}`;
+            const fileContent = await FileSystem.readAsStringAsync(itemPath, { encoding: FileSystem.EncodingType.Base64 });
+            zip.file(item, fileContent, { base64: true });
+          }
+        }
+      }
+
+      const zipContent = await zip.generateAsync({ type: 'base64' });
+      const tempZipPath = FileSystem.cacheDirectory + "sharedFiles.zip";
+      await FileSystem.writeAsStringAsync(tempZipPath, zipContent, { encoding: FileSystem.EncodingType.Base64 });
+
+      await Sharing.shareAsync(tempZipPath);
+
+      // Cleanup
+      await FileSystem.deleteAsync(tempZipPath, { idempotent: true });
+    } catch (error) {
+      console.error(`Failed to share files`, error);
+    }
+    setSelectedItems([]);
   };
 
   useEffect(() => {
@@ -74,32 +121,74 @@ const Files = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        {directories.map((directory, index) => (
-          <TouchableOpacity
-            key={index}
-            style={styles.buttonContainer}
-            onPress={() => handleDirectoryClick(directory.path)}
-          >
-            <Text style={styles.heading}>{directory.name}</Text>
-            <Text style={styles.metadata}>{directory.recordingLength}</Text>
-            <Text style={styles.metadata}>{directory.recordingDate}</Text>
-          </TouchableOpacity>
-          
-        ))}
-        <TouchableOpacity onPress={deleteAllFiles}>
-          <Text>Delete All Files</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <MenuProvider>
+      <SafeAreaView>
+        <ScrollView>
+          <View style={styles.container}>
+            <View style={styles.menuContainer}>
+              <Menu>
+                <MenuTrigger>
+                  <Ionicons name="ellipsis-vertical" size={32} color="black" />
+                </MenuTrigger>
+                <MenuOptions>
+                  <MenuOption onSelect={handleMenuDelete} text='Delete' />
+                  <MenuOption onSelect={() => alert(`Rename`)} text='Rename' />
+                  <MenuOption onSelect={handleMenuShare} text='Share' />
+                </MenuOptions>
+              </Menu>
+            </View>
+            {directories.map((directory, index) => (
+              <TouchableOpacity
+                key={index}
+                style={[
+                  styles.buttonContainer,
+                  selectedItems.includes(directory.path) && styles.selectedButtonContainer
+                ]}
+                onPress={() => handleDirectoryClick(directory.path)}
+                onLongPress={() => handleLongPress(directory)}
+                delayLongPress={200}
+              >
+                <Text
+                  style={[
+                    styles.heading,
+                    selectedItems.includes(directory.path) && styles.selectedText
+                  ]}
+                >
+                  {directory.name}
+                </Text>
+                <Text
+                  style={[
+                    styles.metadata,
+                    selectedItems.includes(directory.path) && styles.selectedText
+                  ]}
+                >
+                  {directory.recordingLength}
+                </Text>
+                <Text
+                  style={[
+                    styles.metadata,
+                    selectedItems.includes(directory.path) && styles.selectedText
+                  ]}
+                >
+                  {directory.recordingDate}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </MenuProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: "5%",
+    marginHorizontal: "2%",
     marginVertical: "10%",
+  },
+  menuContainer: {
+    alignItems: 'flex-end',
+    marginBottom: 10,
   },
   buttonContainer: {
     margin: 10,
@@ -109,6 +198,10 @@ const styles = StyleSheet.create({
     borderColor: "black",
     backgroundColor: "black",
     padding: 15,
+  },
+  selectedButtonContainer: {
+    backgroundColor: "grey",
+    borderColor: "grey",
   },
   heading: {
     color: "white",
@@ -121,6 +214,9 @@ const styles = StyleSheet.create({
     color: "grey",
     textAlign: "left",
     fontFamily: "IBMPlexMono-Medium",
+  },
+  selectedText: {
+    color: "black",
   },
 });
 
